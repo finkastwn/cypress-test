0515d9ff0e049c7db438888825dd4b796a9f78ee
Ipan Suryadi <ipansuryadi@gmail.com>
2024-01-05 19:17:17 +0700

    feat: test patch post - no test found

---
+++ cypress/e2e/apiPatchPost.spec.ts
@@ -0,0 +1,5 @@
+// i want to test patch api from website https://jsonplaceholder.typicode.com/
+// 1. i create this describe
+// and run cy:open then test
+// the result is No tests found.
+describe("Api Patch Post", () => {});
