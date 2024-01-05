d98cfd604c71f9c95854d27332aa1304c64174a9
Ipan Suryadi <ipansuryadi@gmail.com>
2024-01-05 19:54:50 +0700

    feat: expect equal

--- cypress/e2e/apiPatchPost.spec.ts
+++ cypress/e2e/apiPatchPost.spec.ts
@@ -2,4 +2,14 @@
 // 1. i create this describe
 // and run cy:open then test
 // the result is No tests found.
-describe("Api Patch Post", () => {});
+describe("Api Patch Post", () => {
+  // this test is success
+  it("kalau ekspektasi sesuai realita", () => {
+    expect(true).to.equal(true);
+  });
+
+  // this test is failed
+  it("kalau ekspektasi tidak sesuai realita", () => {
+    expect(true).to.equal(false);
+  });
+});
