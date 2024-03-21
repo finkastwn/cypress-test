f7668cb6a1ceaedabae277c1f29e5fd52d6b3eba
finkastwn <winafinkastwn@gmail.com>
2024-03-22 06:45:56 +0700

    chore: remove writeFile to store bookingId to json file

--- cypress/e2e/apiTest.spec.ts
+++ cypress/e2e/apiTest.spec.ts
@@ -1,18 +1,9 @@
 const dataTestPath = `cypress/fixtures/example.json`
 
 describe('api booking', () => {
-    it('create booking', () => {
-        cy.readFile(dataTestPath).then((data) => {
-            data.payload.firstname = "Aaron"
-            data.payload.lastname = "Ramsey"
-            data.payload.totalprice = 150
-            data.payload.depositpaid = true
-            data.payload.bookingdates.checkin = "2023-08-01"
-            data.payload.bookingdates.checkout = "2023-08-02"
-            data.payload.additionalneeds = "Queen Bed"
-            cy.writeFile(dataTestPath, data)
-        })
+    let bookingId: number;
 
+    it('create booking', () => {
         cy.readFile(dataTestPath).then((data) => {
             let payload = data.payload
             cy.request({
@@ -35,8 +26,7 @@ describe('api booking', () => {
                 expect(response.body.booking.bookingdates.checkin).to.eq(data.payload.bookingdates.checkin)
                 expect(response.body.booking.bookingdates.checkout).to.eq(data.payload.bookingdates.checkout)
                 expect(response.body.booking.additionalneeds).to.eq(data.payload.additionalneeds)
-                data.bookingId = response.body.bookingid
-                cy.writeFile(dataTestPath,data)
+                bookingId = response.body.bookingid
             });
         })
     });
@@ -44,7 +34,6 @@ describe('api booking', () => {
     it('get booking', () => {
         // using commands
         cy.readFile(dataTestPath).then((data) => {
-            let bookingId = data.bookingId
             cy.getBooking(bookingId).then((response) => {
                 expect(response.status).to.eq(200)
                 expect(response.statusText).to.eq("OK")
