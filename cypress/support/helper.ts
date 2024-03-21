d7363898f70b6fbc8c1917e00eada27f480c445c
finkastwn <winafinkastwn@gmail.com>
2024-03-22 06:46:36 +0700

    chore: using Chainable<string> for 'result' datatype instead of any

--- cypress/support/helper.ts
+++ cypress/support/helper.ts
@@ -1,8 +1,8 @@
 export const assertUrl = (page: string) => {
-    let result : any
-    if(page == "home"){
-        return result = cy.url().should('eq','https://primaku.com/')
-    }else if(page == "about"){
-        return result = cy.url().should('eq','https://primaku.com/about')
+    let result: Cypress.Chainable<string>
+    if (page == "home") {
+        return result = cy.url().should('eq', 'https://primaku.com/')
+    } else if (page == "about") {
+        return result = cy.url().should('eq', 'https://primaku.com/about')
     }
 }
